43db8a259ec63cc9bc74a1d2e727ccee137ffe0d
Enderlook <enderlook@gmail.com>
2021-05-23 14:06:23 -0300

    Add capability to leave play

--- Assets/Script/Level/Disconnecter.cs
+++ Assets/Script/Level/Disconnecter.cs
@@ -10,9 +10,19 @@ namespace Game.Level
 #pragma warning disable CS0649
         [SerializeField, Tooltip("Name of the main menu scene.")]
         private string mainMenuScene;
+
+        [SerializeField, Tooltip("Key used to surrender.")]
+        private KeyCode leaveKey;
 #pragma warning restore CS0649
 
         private bool leaving;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Update()
+        {
+            if (Input.GetKeyDown(leaveKey))
+                Exit();
+        }
 
         public void Exit()
         {
